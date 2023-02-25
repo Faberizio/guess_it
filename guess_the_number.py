f6e42218157420cb696f3e85ce1d1e280288f95a
Jose Escauriza <escaurizajose@gmail.com>
2023-02-25 18:06:52 +0100

    Stylized user interface

--- guess_the_number.py
+++ guess_the_number.py
@@ -6,13 +6,14 @@ while play == "1":
     # generate a random number between 1 and 10
     secret_number = random.randint(1, 10)
     for attempt in range(1, 4):
-        guess = input("Guess the number (between 1 and 10) in 3 attempts: ")
+        guess = input(
+            "Guess the number (between 1 and 10) in 3 attempts: ")
         if guess.isdigit():  # check if the input is a digit
             guess = int(guess)
             if guess == secret_number:
-                print("Congratulations! You guessed the number!")
+                print("\033[0;32m Congratulations! You guessed the number!")
                 play = input(
-                    "Wanna play again? (1 for yes, any other key to quit): ")
+                    "\033[1;5m               PLAY AGAIN?\n" "\033[0;32m (Press 1 to play again,\033[0;0m press any other key to quit): ")
                 break  # exit the for loop if the player guessed correctly
 
             elif guess > secret_number:
@@ -22,5 +23,6 @@ while play == "1":
         else:
             print("Please enter a number")
     else:
-        print("Game Over. The number was", secret_number)
-        play = input("Wanna play again? (1 for yes, any other key to quit): ")
+        print("\033[0;31mGame Over. The number was", secret_number)
+        play = input(
+            "\033[1;5m               PLAY AGAIN?\n" "\033[0;32m (Press 1 to play again,\033[0;0m press any other key to quit): ")
