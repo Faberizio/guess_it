fc7d56a66f30ddbea38dca1bfb67e9e3280757cd
Fabrizio Golino <info.fabriziogolino@gmail.com>
2023-02-25 13:21:06 +0100

    add guess_the_number.py

---
+++ guess_the_number.py
@@ -0,0 +1,28 @@
+# is a module that provides functions for generating random numbers.
+import random
+
+play = "1"
+
+while play == "1":
+    # generate a random number between 1 and 10
+    secret_number = random.randint(1, 10)
+    for attempt in range(1, 4):
+        guess = input("Guess the number (between 1 and 10) in 3 attempts: ")
+        if guess.isdigit():  # check if the input is a digit
+            guess = int(guess)
+            if guess == secret_number:
+                print("Congratulations! You guessed the number!")
+                play = input(
+                    "Wanna play again? (1 for yes, any other key to quit): ")
+                break  # exit the for loop if the player guessed correctly
+
+            elif guess > secret_number:
+                print("Try again with a lower number")
+            elif guess < secret_number:
+                print("Try again with a greater number")
+        else:
+            print("Please enter a number")
+    else:
+        print("Game Over. The number was", secret_number)
+        play = input("Wanna play again? (1 for yes, any other key to quit): ")
+#
